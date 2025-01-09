491c22c9184dd1e2b37edeaa59a933f2b0041c14
AkshayPatel2004 <akshayhanath53@gmail.com>
2025-01-10 02:10:07 +0530

    veriable In java

---
+++ Veriable.java
@@ -0,0 +1,16 @@
+public class Veriable {
+     public static void main(String[] args) {
+        System.out.println("Veriable In Java");
+        int number = 34;
+        float price = 599.99f;
+        double Salary = 65003;
+        boolean result = false;
+        char alphabet = 'a';
+        System.out.println("Number: "+number);
+        System.out.println("Price: "+price);
+        System.out.println("Amount: "+Salary);
+        System.out.println("Result: "+result);
+        System.out.println("Alphabet: "+alphabet);
+
+     }
+}
