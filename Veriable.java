6acc52156cd91040c79686e52d3cd6fcf1a0fb04
AkshayPatel2004 <akshayhanath53@gmail.com>
2025-01-10 02:15:40 +0530

    new veriable added

--- Veriable.java
+++ Veriable.java
@@ -4,12 +4,14 @@ public class Veriable {
         int number = 34;
         float price = 599.99f;
         double Salary = 65003;
+        long ID = 2344433;
         boolean result = false;
         char alphabet = 'a';
         System.out.println("Number: "+number);
         System.out.println("Price: "+price);
         System.out.println("Amount: "+Salary);
         System.out.println("Result: "+result);
+        System.out.println("Id: "+ID);
         System.out.println("Alphabet: "+alphabet);
 
      }
